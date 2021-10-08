1a16ec0b343a4d9ce0fc12e4b00dd32d9c962ac4
nampdn <nampdn@protonmail.com>
2021-10-08 14:25:53 +0700

    build(scripts): pack every build

--- scripts/prepack.sh
+++ scripts/prepack.sh
@@ -6,7 +6,9 @@ set -euo pipefail
 # cross platform `mkdir -p`
 node -e 'fs.mkdirSync("build/contracts", { recursive: true })'
 
+set -x;
 find artifacts/contracts/ -type f -name '*.json' | xargs -I % sh -c 'cp % build/contracts'
 rm build/contracts/*.dbg.json
+set +x;
 
 node scripts/remove-ignored-artifacts.js
--- scripts/prepare-contracts-package.sh
+++ scripts/prepare-contracts-package.sh
@@ -4,19 +4,18 @@
 cd "$(git rev-parse --show-toplevel)"
 
 # avoids re-compilation during publishing of both packages
-if [ ! -z "${ALREADY_COMPILED}"]; then
-  echo "Compiled, skip do it again!"
-else
-  npm run clean
-  npm run prepare
-  npm run prepack
-fi
-
-# Clean up
-rm -rf contracts/build
+# if [ ! -z "${ALREADY_COMPILED}"]; then
+#   echo "Compiled, skip do it again!"
+# else
+  # npm run clean
+  # npm run prepare
+  # npm run prepack
+# fi
 
 # Move artifacts
+set -x;
 cp README.md contracts/
-# mkdir contracts/build
+mkdir -p contracts/build/{contracts,dist}
 cp -r build/contracts/*.json contracts/build/contracts
+set +x;
 # cp -r dist/typechain/* contracts/build/typechain
